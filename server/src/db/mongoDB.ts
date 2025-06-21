1ed531e9dd588f84dfd573490b53d2b3ca6f6be8
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-06-21 17:44:15 +0800

    fix redundancy

--- server/src/db/mongoDB.ts
+++ server/src/db/mongoDB.ts
@@ -1,23 +1,16 @@
 import { MongoClient } from "mongodb";
-import type { Db } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 if (!uri) throw new Error("Missing MONGODB_URI");
 
 const client = new MongoClient(uri);
-let db: Db | null = null;
 
 export async function connectDB() {
-    if (db) return db;
-
     try {
         await client.connect();
-        db = client.db();
         console.log("MongoDB connected");
-        return db;
     } catch (err) {
         console.error("Failed to connect to MongoDB:", err);
-        db = null
         throw err
     }
 }
